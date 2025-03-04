dc3c12ab26db0ddd56af8e572ebd22d6e8ab5c8f
brian d foy <briandfoy@pobox.com>
2025-03-04 14:16:21 -0500

    initial creation

---
+++ lib/App/ipinfo.pm
@@ -0,0 +1,65 @@
+package App::ipinfo;
+use strict;
+
+use warnings;
+no warnings;
+
+our $VERSION = '0.001_01';
+
+=encoding utf8
+
+=head1 NAME
+
+App::ipinfo - A command-line tool for IPInfo.io
+
+=head1 SYNOPSIS
+
+	use App::ipinfo;
+
+=head1 DESCRIPTION
+
+=over 4
+
+=item new
+
+=cut
+
+sub new {
+
+	}
+
+=item init
+
+=cut
+
+sub init {
+
+	}
+
+=back
+
+=head1 TO DO
+
+
+=head1 SEE ALSO
+
+
+=head1 SOURCE AVAILABILITY
+
+This source is in Github:
+
+	http://github.com/briandfoy/app-ipinfo
+
+=head1 AUTHOR
+
+brian d foy, C<< <briandfoy@pobox.com> >>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright © 2025, brian d foy, All Rights Reserved.
+
+You may redistribute this under the terms of the Artistic License 2.0.
+
+=cut
+
+1;
